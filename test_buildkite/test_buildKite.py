5eccd5038c45fed6f628240f91ee9ca55ca1b005
karankagarwal <karan.agarwal@bluecore.com>
2022-09-25 14:22:53 +0530

    Adding 2 TC to the build

---
+++ test_buildkite/test_buildKite.py
@@ -0,0 +1,8 @@
+
+class TestBuildKite:
+
+    def test_testcase1(self):
+        assert 2==2, "Test Case Failed, number comparison didnt matched"
+
+    def test_testcase2(self):
+        assert 2 == 3, "Test Case Failed, number comparison didnt matched"
